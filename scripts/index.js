31ec8d7b33c6d94a545b0705df80c8d2e3d9fc8b
Jamie Socorro <jamie_socorro@live.com>
2018-09-04 01:41:13 -0600

    added function for pointer down event

--- scripts/index.js
+++ scripts/index.js
@@ -47,6 +47,64 @@ window.addEventListener("DOMContentLoaded", function () {
         pad3.material = material3;
         pad4.material = material4;
 
+        var animateBox = new BABYLON.Animation("boxPressed", "position.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+        var animateCol = new BABYLON.Animation("boxPressed", "material.emissiveColor", 30, BABYLON.Animation.ANIMATIONTYPE_COLOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+        
+        // An array with all animation keys
+        var keys = [];
+
+        //At the animation key 0, the value of scaling is "1"
+        keys.push({
+            frame: 0,
+            value: 0
+        });
+
+        //At the animation key 0, the value of scaling is "0.2"
+        keys.push({
+            frame: 5,
+            value: -0.2
+        });
+
+
+        //At the animation key 0, the value of scaling is "1"
+        keys.push({
+            frame: 10,
+            value: 0
+        });
+
+         // An array with all animation keys
+        var keysC = [];
+
+        //At the animation key 0, the value of scaling is "1"
+        keysC.push({
+            frame: 0,
+            value: new BABYLON.Color3(0,0,0)
+        });
+
+        //At the animation key 0, the value of scaling is "1"
+        keysC.push({
+            frame: 5,
+            value: new BABYLON.Color3(.7,.7,1)
+        });
+
+        //At the animation key 0, the value of scaling is "1"
+        keysC.push({
+            frame: 10,
+            value: new BABYLON.Color3(0,0,0)
+        });
+
+        animateBox.setKeys(keys);
+        animateCol.setKeys(keysC);
+
+        scene.onPointerDown = function (evt, pickResult) {
+            if(pickResult.hit) {
+                pickResult.pickedMesh.animations.push(animateBox);
+                pickResult.pickedMesh.animations.push(animateCol);
+
+                scene.beginAnimation(pickResult.pickedMesh, 0, 10, true);
+            }
+        };
+
         return scene;
     }
 
